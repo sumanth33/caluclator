e1f8d708d4ac7c2483f02025a086662c8b20c11d
sumanth33 <53785304+sumanth33@users.noreply.github.com>
2021-05-16 16:00:26 +0530

    a simple caluclator

---
+++ caluclator .py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+x = int(input("enter the key 1 "))
+y = int(input("enter the key 2 "))
+z = input("enter the operator")
+if z == "+":
+    print( x+y)
+elif z == "-" :
+    print (x-y) if y<x else y-x 
+elif z == "*" :
+    print (x*y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
